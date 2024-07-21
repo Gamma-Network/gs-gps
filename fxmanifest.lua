e827947ab68e78786931f449faa5a18d4ecccc6d
FalconHH <103220346+FalconHH@users.noreply.github.com>
2024-07-21 23:16:54 +0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'GammaScript'
+description 'GPS'
+version '1.0'
+
+client_scripts {
+    "client/main.lua"
+}
+
+server_scripts {
+    "server/main.lua"
+}
+
+shared_scripts {
+    "shared/config.lua",
+    "shared/locales.lua"
+}
