a907d0c018d9b0b8f2641fc690dc7dfb9b79922b
FalconHH <103220346+FalconHH@users.noreply.github.com>
2024-07-21 23:16:32 +0400

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,32 @@
+Config = {}
+
+Config.Locales = {
+    gps_item_check = "GPS item:",
+    input_header = "GPS Number and Department Selection",
+    submit_text = "Submit",
+    gps_number_prompt = "Enter GPS Number",
+    department_prompt = "Select Department",
+    select_department = "Select Your Department",
+    not_police_or_doctor = "You are not a Police Officer or Doctor",
+    no_gps = "No GPS found!",
+    gpsOpen = "GPS Opened",
+    gpsClose = "GPS Closed",
+    gpsError = "GPS Couldn't Open",
+    jobDutyMessage = "You were taken off duty because you were injured"
+}
+
+Config.BlipSettings = {
+    normal_blip_scale = 0.85,
+    flashing_blip_scale = 0.6,
+    dead_blip_scale = 1.0,
+    police_blip_scale = 0.3
+}
+
+Config.PoliceDepartments = {
+    { value = "pd", text = "LSPD" },
+    { value = "sd", text = "BCSO" }
+}
+
+Config.EMSDepartment = {
+    { value = "ems", text = "EMS" }
+}
